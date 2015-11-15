23a91b52fe3562ab6e4fae8e8fc927fc2a0b6091
Tom Hay <thomasghay@gmail.com>
2015-11-15 16:25:51 -0500

    pod updates

---
+++ Just BeTests/Just_BeTests.m
@@ -0,0 +1,39 @@
+//
+//  Just_BeTests.m
+//  Just BeTests
+//
+//  Created by Thomas Hay on 11/15/15.
+//  Copyright © 2015 Group 2. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Just_BeTests : XCTestCase
+
+@end
+
+@implementation Just_BeTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
